3d2a858176d00cd27c105a6c1341a3bd622d0f53
Mateo Revilla <mateorevillalecca@gmail.com>
2020-11-11 00:04:26 -0500

    Setup React Router, CSS, React Bootsrap, and look to UI

---
+++ src/pages/Landing.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import PageTitle from '../components/PageTitle'
+import { useHistory } from 'react-router-dom';
+
+function Landing() {
+
+    let history = useHistory();
+    return (
+        <div >
+            <PageTitle title="Poker Platform" />
+            <Container fluid className="containerOptions">
+                <Row className="optionsRow">
+                    <Col lg={{ span: 4, offset: 4 }}>
+                        <button onClick={() => {
+                            history.push("/create");
+                        }}
+                            className="optionsButton">
+                            Create Game
+                    </button>
+                    </Col>
+
+
+                </Row>
+
+                <Row>
+                    <Col lg={{ span: 4, offset: 4 }}>
+                        <button onClick={() => {
+                            history.push("/login");
+                        }} className="optionsButton">
+                            aaa
+                        </button>
+                    </Col>
+                </Row>
+
+            </Container>
+
+        </div>
+    );
+}
+
+
+
+export default Landing;
---
+++ src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle'
+
+function Login() {
+    return (
+        <PageTitle title = "Login"/>
+    );
+}
+
+export default Login;
