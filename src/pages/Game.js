6e336bd4e59a566ad1336ee8f47ef9159582e08e
ando-huang <ahuang09182000@gmail.com>
2020-11-26 17:08:07 -0500

    started adding socketio and card ui

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -24,7 +24,14 @@ function Game() {
         <Row>
           <Col lg={{ span: 8, offset: 2 }}>
             <div className="gameStyles">
-              <label className="gameTable">TABLE</label>
+              <label className="gameTable">TABLE
+                <Deck
+                  board={["3s", "Qh", "As"]} // array of cards to render on board
+                  boardXoffset={375} // X axis pixel offset for dealing board
+                  boardYoffset={200} // Y axis pixel offset for dealing board
+                  size={200} // card height in pixels
+                />
+              </label>
             </div>
           </Col>
         </Row>
--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PageTitle from "../components/PageTitle";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
