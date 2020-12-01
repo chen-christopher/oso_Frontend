dbf1157a0f0f023c53becfd926cd23d8033f3725
ando-huang <ahuang09182000@gmail.com>
2020-12-01 08:41:29 -0500

    test code emit

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import PageTitle from "../components/PageTitle";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { ReactComponent as Logo } from "./return.svg";
 import { useHistory } from "react-router-dom";
+import socketIOClient from "socket.io-client"; 
+
+const socket = socketIOClient('http://localhost:3000', {
+  transports: ['websocket'],
+  autoConnect: false,
+})
 
 function Create() {
   let history = useHistory();
+  const [connectionStatus, setConnectionStatus] = useState(false)
+  const [time, setTime] = useState([])
+  const [channel, setChannel] = useState()
+
   var code = Math.floor(100000 + Math.random() * 900000); //generate this from the db
+  //send code through socket
+  socket.on("connection", function(){ //im not actually sure what this does
+    socket.emit('code', { code: code });
+  })
+
   return (
     <div>
       <PageTitle title="New Game" />
@@ -23,7 +38,7 @@ function Create() {
             <Col lg={{ span: 4, offset: 4 }}>
                 <label class='optionsButton'>
                     Username:
-                    <input class = "textEntry" type="text" /> 
+                    <input class = "textEntry" type="text"/> 
                 </label>                       
             </Col>
         </Row>
