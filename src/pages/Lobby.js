060c2186ca9f3134e6b3aa5a6bce77cb4910aec4
Chris <pcc386@nyu.edu>
2020-12-01 23:52:20 +0400

    added cards

--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import PageTitle from "../components/PageTitle";
 import Container from "react-bootstrap/Container";
@@ -6,64 +5,49 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { ReactComponent as Logo } from "./return.svg";
 import { useHistory, useLocation } from "react-router-dom";
-import socketIOClient from 'socket.io-client'; 
+import socketIOClient from "socket.io-client";
 
-const socket = socketIOClient('http://localhost:3000', {
-  transports: ['websocket'],
+const socket = socketIOClient("http://localhost:3000", {
+  transports: ["websocket"],
   autoConnect: false,
-})
-
-
-
-
+});
 
 function Lobby() {
   let history = useHistory();
   const location = useLocation();
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-  socket.open()
-  socket.on('connect', () => {
-    console.log("CONNECTED")
-  })
+  socket.open();
+  socket.on("connect", () => {
+    console.log("CONNECTED");
+  });
 
-  socket.on('lobby', (data) => { 
-    setUsers(data)
-  })
-  
+  socket.on("lobby", (data) => {
+    setUsers(data);
+  });
 
-
-  
   return (
     <div>
-    
       <PageTitle title="Game Lobby" />
-      
-    
+
       <Container fluid className="lobbyContainer">
+        {users &&
+          users.map((user) => (
+            <p>
+              <Row>
+                <Col lg={{ span: 4, offset: 4 }}>
+                  <label className="lobbyDiv">{user}</label>
+                </Col>
+              </Row>
+            </p>
+          ))}
 
-      {users && users.map((user) => <p>
-        <Row>
-          <Col lg={{ span: 4, offset: 4 }}>
-            <label className="lobbyDiv"> 
-            
-            {user}
-            
-            </label>
-            
-          </Col>
-        </Row>
-      
-      </p>)}
-
-        
-
-        
         <Row>
           <div className="flexRow">
             <div>
               <label className="lobbyDiv" id="codeDisplayButton">
-                Code: {location.state.table_id  }
+                Code:
+                {/* Code: {location.state.table_id  } */}
               </label>
             </div>
 
